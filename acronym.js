5d55360bb5cde98de5beb7b7774572b3be99555e
Vagner Dutra <vagnerdutra@outlook.com>
2015-03-28 04:52:29 -0300

    acrescimo de id nas tags acronym

--- acronym.js
+++ acronym.js
@@ -2,25 +2,22 @@
 
 	function acronym(){
 		for (var k = 1, tam1 = $(".acronym").length; k <= tam1; k++) {
-
 			var content_ini = $(".acronym:nth-child("+k+")").html();
+			$(".acronym:nth-child("+k+")").attr('id', 'acronym'+k);
 			var content_end = "";
 			var pals = content_ini.split(" ");
 			for (var i = 0, tam = pals.length; i<tam; i++) {
-				content_end += "<span id='acronym"+k+"' class='pal'>"+pals[i]+"</span>";
+				content_end += "<span class='pal'>"+pals[i]+"</span>";
 			};
-
 			$(".acronym:nth-child("+k+")").html(content_end);
 		}
 	}
-
-
 
 	$(document).ready(function(){
 		acronym();
 		$('.acronym').hover(
 			function () {
-				$(".pal").toggleClass("palHover"); 
+				$("#"+this.id+" .pal").toggleClass("palHover"); 
 			}
 		);
 	});
